621992a21c18c12c590680e280ef9bbd556356b0
DoAZip <155206180+RafaelD135@users.noreply.github.com>
2025-10-08 14:34:25 +0000

    Création Q6.java

---
+++ Q6.java
@@ -0,0 +1,15 @@
+public class Q6 {
+    public static void afficherTriangle(int N) {
+        for (int i = N; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print('x' + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        afficherTriangle(N);
+    }
+}
